4b34993bef5f35463ebac8bf7880759aed528b95
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-09-30 08:54:31 +0300

    create update.py

---
+++ updater/update.py
@@ -0,0 +1,46 @@
+""" 
+This script will update the program
+"""
+
+import os
+import time
+import requests
+import json
+
+try:
+    import gdown
+except ImportError as e:
+    os.system('pip install gdown')
+
+print("""
+======================================
+
+        Dcnapp Update Script
+
+======================================
+
+This script will update dcnapp by
+
+Downloading zip file from google drive
+And clean files for you 
+The config files for this program
+in the github repo 
+You can access the repo by visiting : https://github.com/Senpai-10/DCNAPP 
+
+""")
+
+
+class update:
+
+    def clean(self):
+        ...
+
+update = update()
+
+data = requests.get('')
+jsondata = data.json()
+
+id = '1ukI1C4XGRHeCmCVwNoudv_4wG1JYTkgT'     # read from config file
+url = 'https://drive.google.com/uc?id=' + id
+output = 'spam.png'
+gdown.download(url, output, quiet=False) 
