06ae818bb00b256e8aa9af5e25d715e2fca98b0e
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-10-06 13:17:43 +0300

    +getvalue + setvalue

--- src/web/js/logic.js
+++ src/web/js/logic.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
     $("button").click(function() {
-        console.log('button has been clicked');
         $("#frame").attr("src", $(this).attr("value"));
     })
+
+    $("#list").click(function() {
+        $(".dropdown-list").toggle();
+    });
 });
+
+function SetValue() {
+    //
+}
+
+function GetValue(n) {
+    // var word = document.getElementsByClassName('ep');
+    var word = $(".ep").text();
+
+    if (word.search("1")) {
+        var URL = 'https://mega.nz/embed/Th50WQ5Q#yKTVGui8_A0eDnHnwYdckJG4tfLrfPqSflF62DFt5bc';
+
+    }
+    
+}
+const url1 = '';
