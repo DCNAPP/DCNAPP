1d11b4e7c8f2053dd182ebd6421e9651759e3495
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-10-12 21:40:34 +0300

    remove while loop

--- src/main.py
+++ src/main.py
@@ -6,23 +6,9 @@ import time
 # access the web folder
 eel.init('web')
 # run the html file 
-eel.start('index.html', block=False)
-
-# check the network Connection
-# if false open html file display network error 
-def Connection_Test():
-    """Check The Network Connection"""
-    try:
-        socket.create_connection(('Google.com', 80))
-        return True
-
-    except OSError:
-        os.system('start no-connection.html')
-        return False
+eel.start('index.html')
+# eel.start('index.html', block=False)
 
 
-# call the Connection_Test function
-Connection_Test()
-
-while True:
-    eel.sleep(10)
+# while True:
+#     eel.sleep(10)
