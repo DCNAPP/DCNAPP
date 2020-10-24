39736afbf31ad01d792877f188773c7af48be240
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-10-24 17:51:53 +0300

    init exit function

--- src/main.py
+++ src/main.py
@@ -6,9 +6,18 @@ import time
 # access the web folder
 eel.init('web')
 # run the html file 
-eel.start('index.html')
-# eel.start('index.html', block=False)
+# eel.start('index.html')
+eel.start('index.html', block=False)
+
+@eel.expose
+def exit_py():
+    '''
+    exit() is fixing:
+        after you close dcnapp the program is still running in the background
+    
+    '''
+    exit()
 
 
-# while True:
-#     eel.sleep(10)
+while True:
+    eel.sleep(10)
