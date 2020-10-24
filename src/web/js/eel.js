76e776634a2e2f7373beb50c684b79cdbd7f28ad
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-10-24 17:54:00 +0300

    change episode title

---
+++ src/web/js/eel.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#list").click(function() {
+        $(".dropdown-list").toggle();
+    });
+    
+});
+
+var version = "2020.1.0";
+
+
+$(document).ready(function(){
+    $.getJSON('https://senpai-10.github.io/DCNAPP/versions/version.json', function SetValue(data) {
+            
+
+        
+        if (version == data['last-version']) {
+
+            document.getElementById("update-button").style.backgroundColor = '#c5c5c5';
+            document.getElementById("update-button").style.cursor = 'not-allowed';
+            document.getElementById("update-a").href = 'none';
+        }
+        else {
+            document.getElementById("update-button").style.backgroundColor = 'rgb(37, 221, 77)';
+        }
+
+    });
+});
+
+function SetValue(n) {
+    var word = $(".ep").text();
+    if (word.search(n)) {
+        // https://senpai-10.github.io/DCNAPP/urls.json
+        $.getJSON('https://senpai-10.github.io/DCNAPP/urls.json', function SetValue(data) {
+            
+            var element = document.querySelector('#frame');
+            element.src = data[n];
+
+        });
+        
+        document.getElementById("ep-title").innerHTML = "الحلقة رقم " + n;
+    }
+}
+
+
+function GetValue(n) {
+    SetValue(n);
+}
+
