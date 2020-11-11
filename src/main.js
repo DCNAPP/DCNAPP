57d52426042f430ac61c042bc2c97f7c52dcde79
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-11-11 04:57:33 +0300

    change loadURL

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
+let version = 'v1.4.0';
 
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const url = require("url");
+const fs = require('fs');
 
 let window;
 
 function createWindow() {
     window = new BrowserWindow();
-    window.loadURL(url.format({
-        pathname: path.join("UI", "index.html"),
-        protocol: 'file',
-        slashes: true
-    }));
+    window.maximize();
+    // window.loadURL(url.format({
+    //     pathname: path.join(__dir, "index.html"),
+    //     protocol: 'file',
+    //     slashes: true
+    // }));
+    window.loadURL('file://' + __dirname + '/index.html')
 
     window.on("closed", () => {
         window = null;
@@ -22,3 +26,10 @@ function createWindow() {
 
 
 app.on('ready', createWindow);
+
+fs.writeFile('version.txt', version, (err) => {
+
+    if (err) throw err;
+
+});
+
