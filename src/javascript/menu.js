538b55d3c9461f6d1e93f70b6e464948ab9385ee
senpai-10 <53495583+Senpai-10@users.noreply.github.com>
2020-11-21 02:30:00 +0300

    functionality to the menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -8,51 +8,59 @@ $(document).ready(function(){
     
 
     // open settings
-    $(".elm-body-settings").click(function() {
-        $(".elm-body-settings").hide();
-        $(".elm-body-info").hide();
-        $(".elm-body-issue").hide();
+    $("#elm-body-settings").click(function() {
+        $("#elm-body-settings").hide();
+        $("#elm-body-info").hide();
+        $("#elm-body-issue").hide();
         $("#settings-inside").show('slow');
 
         $("#expand").click(function() {
-            $(".elm-body-settings").show('slow');
-            $(".elm-body-info").show();
-            $(".elm-body-issue").show();
+            $("#elm-body-settings").show('slow');
+            $("#elm-body-info").show();
+            $("#elm-body-issue").show();
             $("#settings-inside").hide();
         });
     });
     // open [settings][theme]
-    $(".elm-body-settings-theme").click(function() {
+    $("#elm-body-settings-theme").click(function() {
         $("#settings-inside").hide();
         $("#settings-inside-theme").show('slow');
 
         $("#expand").click(function() {
-            $(".elm-body-settings").show('slow');
+            $("#elm-body-settings").show('slow');
             $("#settings-inside-theme").hide();
         });
         
     });
 
-    $(".elm-body-issue").click(function() {
-        window.location = "https://github.com/Senpai-10/DCNAPP/labels/bug";
-    });
+    
     
 
-    $(".elm-body-info").click(function() {
+    $("#elm-body-info").click(function() {
         $('#version').text('version: ' + version)
-        $(".elm-body-settings").hide();
-        $(".elm-body-info").hide();
-        $(".elm-body-issue").hide();
+        $("#elm-body-settings").hide();
+        $("#elm-body-info").hide();
+        $("#elm-body-issue").hide();
         $("#info-inside").show('slow');
 
         $("#expand").click(function() {
-            $(".elm-body-settings").show('slow');
-            $(".elm-body-info").show();
-            $(".elm-body-issue").show();
+            $("#elm-body-settings").show('slow');
+            $("#elm-body-info").show();
+            $("#elm-body-issue").show();
             $("#info-inside").hide();
         });
     });
     
+    $("#elm-body-settings-theme-in-dark").click(function() {
+        localStorage.setItem('theme', 'dark');
+        $("#menu-body").hide('slow');
+        location.reload();
+    });
+    $("#elm-body-settings-theme-in-light").click(function() {
+        localStorage.setItem('theme', 'light');
+        $("#menu-body").hide('slow');
+        location.reload();
+    });
     
-
+    
 });
