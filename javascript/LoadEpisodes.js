69e6e4308d160e8a2fb1bb7aee25c9ab38994d38
senpai-10 <bmjfdrh@gmail.com>
2021-05-25 11:08:16 +0300

    LoadEpisodes.js

--- javascript/LoadEpisodes.js
+++ javascript/LoadEpisodes.js
@@ -1,20 +1,45 @@
-$.getJSON('https://dcnapp.github.io/Config/EpisodesData.json', data => {
-    var count = data.length;
-    var container = document.getElementById("dropdown-list");
-    var EpisodeElement;
+if (localStorage.getItem("filter") == "true") {
+    $.getJSON('https://dcnapp.github.io/Config/EpisodesData.json', data => {
+        var count = data.length;
+        var container = document.getElementById("dropdown-list");
+        var EpisodeElement;
 
-    for (var i = 0; i < count; i++) {
-        var item = data[i];
-        if (item.isfiller === true) {
-            EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}<span class="filler">فلر</span></button></div>`
-        } else {
-            EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+        for (var i = 0; i < count; i++) {
+            var item = data[i];
+            
+            if (item.isfiller == false) {
+                EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+                container.innerHTML += EpisodeElement;
+            }
         }
-        container.innerHTML += EpisodeElement;
+    })
+} else {
+    $.getJSON('https://dcnapp.github.io/Config/EpisodesData.json', data => {
+        var count = data.length;
+        var container = document.getElementById("dropdown-list");
+        var EpisodeElement;
+
+        for (var i = 0; i < count; i++) {
+            var item = data[i];
+            if (item.isfiller === true) {
+                EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}<span class="filler">فلر</span></button></div>`
+            } else {
+                EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+            }
+            container.innerHTML += EpisodeElement;
+        }
+    })
+}
+
+function toggle_filter()
+{
+    if (localStorage.getItem("filter") == "true") {
+        localStorage.setItem("filter", "false")
+    } else {
+        localStorage.setItem("filter", "true")
     }
-});
-
-
+    location.reload()
+}
 
 function LoadEpisodes(url, title) {
     CheckForUpdates()
