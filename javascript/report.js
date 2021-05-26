c5bc6526af19bf2f935b800b5f2018fa552235bb
senpai-10 <bmjfdrh@gmail.com>
2021-05-27 01:56:55 +0300

    hide report button

--- javascript/report.js
+++ javascript/report.js
@@ -14,6 +14,8 @@ function discord_message(webHookURL, message) {
 
 async function episode_not_working(title) {
     discord_message("https://discord.com/api/webhooks/847020859315060806/ncqr_BPyI--HvSd22fg8jkKN-VoH98hMCeI8cix3aM5dSChpppy-_v--ljgTCTWs4Jln", `hello ${title} is not working <@422611986506055681>`)
-    await wait("3s")
+    await wait("1s")
     document.getElementById("report-button").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>`
+    await wait("2s")
+    display.hide("#report-button")
 }
