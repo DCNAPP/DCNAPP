126478dd79ef417ce3a37056c0f3bb60d9a70653
senpai-10 <bmjfdrh@gmail.com>
2021-05-27 01:47:44 +0300

    remove jquery

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,66 +1,58 @@
-$(document).ready(function () {
+docReady(() => {
 
     // toggle expand arrow
-    $("#expand").click(function () {
-        $("#menu-body").slideToggle('slow');
-    });
-
-
+    document.getElementById("expand").addEventListener("click", () => {
+        document.getElementById("expand").classList.toggle("up")
+        display.toggle("#menu-body")
+    })
 
     // open settings
-    $("#elm-body-settings").click(function () {
-        $("#elm-body-settings").hide();
-        $("#elm-body-info").hide();
-        $("#elm-body-issue").hide();
-        $("#settings-inside").show('slow');
+    document.getElementById("elm-body-settings").addEventListener("click", () => {
+        display.hide("#elm-body-settings")
+        display.hide("#elm-body-info")
+        display.show("#settings-inside")
 
-        $("#expand").click(function () {
-            $("#elm-body-settings").show('slow');
-            $("#elm-body-info").show();
-            $("#elm-body-issue").show();
-            $("#settings-inside").hide();
-        });
-    });
+        document.getElementById("expand").addEventListener("click", () => {
+            display.show("#elm-body-settings")
+            display.show("#elm-body-info")
+            display.hide("#settings-inside")
+        })
+    })
+
     // open [settings][theme]
-    $("#elm-body-settings-theme").click(function () {
-        $("#settings-inside").hide();
-        $("#settings-inside-theme").show('slow');
+    document.getElementById("elm-body-settings-theme").addEventListener("click", () => {
+        display.hide("#settings-inside")
+        display.show("#settings-inside-theme")
 
-        $("#expand").click(function () {
-            $("#elm-body-settings").show('slow');
-            $("#settings-inside-theme").hide();
-        });
+        document.getElementById("expand").addEventListener("click", () => {
+            display.show("#elm-body-settings")
+            display.hide("#settings-inside-theme")
+        })
+    })
 
-    });
+    // open [settings][info]
+    document.getElementById("elm-body-info").addEventListener("click", () => {
+        document.getElementById('version').innerHTML = `version: ${version}`
+        display.hide("#elm-body-settings")
+        display.hide("#elm-body-info")
+        display.show("#info-inside")
 
+        document.getElementById("expand").addEventListener("click", () => {
+            display.show("#elm-body-settings")
+            display.show("#elm-body-info")
+            display.hide("#info-inside")
+        })
+    })
 
-
-
-    $("#elm-body-info").click(function () {
-        $('#version').text('version: ' + version)
-        $("#elm-body-settings").hide();
-        $("#elm-body-info").hide();
-        $("#elm-body-issue").hide();
-        $("#info-inside").show('slow');
-
-        $("#expand").click(function () {
-            $("#elm-body-settings").show('slow');
-            $("#elm-body-info").show();
-            $("#elm-body-issue").show();
-            $("#info-inside").hide();
-        });
-    });
-
-    $("#elm-body-settings-theme-in-dark").click(function () {
-        localStorage.setItem('theme', 'dark');
-        $("#menu-body").hide('slow');
-        location.reload();
-    });
-    $("#elm-body-settings-theme-in-light").click(function () {
-        localStorage.setItem('theme', 'light');
-        $("#menu-body").hide('slow');
-        location.reload();
-    });
-
-
-});
+    document.getElementById("elm-body-settings-theme-in-dark").addEventListener("click", () => {
+        localStorage.setItem('theme', 'dark')
+        display.hide("#menu-body")
+        location.reload()
+    })
+    
+    document.getElementById("elm-body-settings-theme-in-light").addEventListener("click", () => {
+        localStorage.setItem('theme', 'light')
+        display.hide("#menu-body")
+        location.reload()
+    })
+})
