463d2b6fb43d6ebc445aea7b9f4e66e0b31460a0
senpai-10 <bmjfdrh@gmail.com>
2021-05-29 07:47:19 +0300

    active ep

--- javascript/LoadEpisodes.js
+++ javascript/LoadEpisodes.js
@@ -9,7 +9,7 @@ if (localStorage.getItem("filter") == "true") {
                 var item = data[i];
                 
                 if (item.isfiller == false) {
-                    EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+                    EpisodeElement = `<div class="dropdown-list_item"><button id="${item.id}" class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\', \'${item.id}\')">${item.title}</button></div>`
                     container.innerHTML += EpisodeElement;
                 }
             }
@@ -25,13 +25,13 @@ if (localStorage.getItem("filter") == "true") {
                 var item = data[i];
                 if (item.isfiller === true) {
                     if (localStorage.getItem("filler-mark") == "filler-tag") {
-                        EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')"><span class="filler-tag">فلر</span>${item.title}</button></div>`
+                        EpisodeElement = `<div class="dropdown-list_item"><button id="${item.id}" class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\', \'${item.id}\')"><span class="filler-tag">فلر</span>${item.title}</button></div>`
                     }
                     else {
-                        EpisodeElement = `<div class="dropdown-list_item"><button class="ep filler-ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+                        EpisodeElement = `<div class="dropdown-list_item"><button id="${item.id}" class="ep filler-ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\', \'${item.id}\')">${item.title}</button></div>`
                     }
                 } else {
-                    EpisodeElement = `<div class="dropdown-list_item"><button class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\')">${item.title}</button></div>`
+                    EpisodeElement = `<div class="dropdown-list_item"><button id="${item.id}" class="ep" onclick="LoadEpisodes(\'${item.url}\', \'${item.title}\', \'${item.id}\')">${item.title}</button></div>`
                 }
                 container.innerHTML += EpisodeElement;
             }
@@ -48,7 +48,11 @@ function toggle_filter()
     location.reload()
 }
 
-function LoadEpisodes(url, title) {
+function LoadEpisodes(url, title, id_number) {
+    if (document.querySelector(".active-ep"))   
+        document.querySelector('.active-ep').classList.remove('active-ep')
+    document.getElementById(`${id_number}`).classList.add("active-ep")
+    
     CheckForUpdates()
     UpdateTitle(title)
     document.querySelector('#frame').src = url
@@ -57,7 +61,7 @@ function LoadEpisodes(url, title) {
 }
 
 const UpdateTitle = (title) => {
-    document.getElementById("report-title-div").innerHTML += `<button id="report-button" title="episode not working!" onclick="episode_not_working('${title}')"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z"/></svg></button>`
+    document.getElementById("report-title-div").innerHTML = `<button id="report-button" title="episode not working!" onclick="episode_not_working('${title}')"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z"/></svg></button>`
     document.getElementById("report-title-div").innerHTML += `<h1 id="ep-title">${title}</h1>`
 }
 
